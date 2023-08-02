88dff02430acfc0e853f0a8b1995f1ac4234de91
victormusembi11 <victor.musembi11@gmail.com>
2023-08-02 22:54:51 +0300

    showing it's not possible to assign int to type str

---
+++ variables.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var greetings = "Hello, World!";
+// greetings = 10; // This will throw an error because greetings is a string
+var mynum = 10;
---
+++ variables.ts
@@ -0,0 +1,10 @@
+let greetings: string = "Hello, World!";
+
+// greetings = 10; // This will throw an error because greetings is a string
+
+
+let mynum = 10;
+
+// mynum.toUpperCase(); // This will throw an error because mynum is a number
+
+export {}; // This is to avoid TS error: "Cannot redeclare block-scoped variable 'greetings'."
