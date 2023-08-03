93134d4fe0676fadd8f93850a470b3089a75e0a5
victormusembi11 <victor.musembi11@gmail.com>
2023-08-03 16:57:49 +0300

    ts enums

---
+++ enums.ts
@@ -0,0 +1,8 @@
+const enum SeatChoice {
+    AISLE = 10,
+    WINDOW = "window",
+    MIDDLE = 20,
+    // FOUTH, // This is not allowed because it's not initialized
+}
+
+const hcSeat: SeatChoice = SeatChoice.AISLE;
