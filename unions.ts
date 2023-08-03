0b5be0ac76e2b63b429cbe6d3f18326c7f5211af
victormusembi11 <victor.musembi11@gmail.com>
2023-08-03 16:02:30 +0300

    declare obj types & using them to define types allowed for a variable

--- unions.ts
+++ unions.ts
@@ -11,3 +11,23 @@ score = [1, 2, 3];
 score = {name: "John Doe", age: 33};
 
 */
+
+type User = {
+    name: string,
+    id: number,
+}
+
+type Admin = {
+    username: string,
+    id: number,
+}
+
+let user: User | Admin = {
+    name: "John Doe", // Currenlty it's a User type
+    id: 1,
+}
+
+user = {
+    username: "johndoe", // It's now changed to an Admin type
+    id: 2,
+}
