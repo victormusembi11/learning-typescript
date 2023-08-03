c0bac977a0c74a2fe0288259f737b37ccdb6bcfd
victormusembi11 <victor.musembi11@gmail.com>
2023-08-03 18:38:22 +0300

    interfaces basics

---
+++ interfaces.ts
@@ -0,0 +1,22 @@
+interface User {
+    readonly _dbId: number,
+    email: string,
+    userId: number,
+    isActive?: boolean,
+    startTrail: () => string,
+    startTrail2:  string,
+    getCoupon(name: string, value: number): number,
+}
+
+const user: User = {
+    _dbId: 22,
+    email: "johndoe@example.com",
+    userId: 2211,
+    startTrail: () => {
+        return "Trail started"
+    },
+    startTrail2: "Trail started",
+    getCoupon: (name: "john10", off: 10) => {
+        return 10;
+    }
+};
