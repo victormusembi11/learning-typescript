25ee008a27022f7baf2f489e499ac186bcce9799
victormusembi11 <victor.musembi11@gmail.com>
2023-08-03 16:51:24 +0300

    ts tuples

---
+++ tuples.ts
@@ -0,0 +1,13 @@
+let user: [string, number, boolean];
+
+// user = [true, "John Doe", 1]; // This is not allowed because the order of the values is not correct
+
+user = ["John Doe", 1, true];
+
+
+let rgb: [number, number, number] = [255, 0, 0];
+
+
+type Employee = [string, number];
+
+export {};
