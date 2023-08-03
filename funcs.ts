e01a82699c1693c47c629052ba05b240e92da007
victormusembi11 <victor.musembi11@gmail.com>
2023-08-03 11:57:30 +0300

    setting type for func arguments

---
+++ funcs.ts
@@ -0,0 +1,25 @@
+function addTwo(num) {
+    // num.toUpperCase(); // if num dosnt have a type, this will not throw an error
+    return num + 2;
+}
+
+/*
+Below will return 102 because the function is treating the argument as a string.
+This is because the function is not expecting a type for the argument.
+*/
+
+addTwo("10"); 
+
+function addThree(num: number) {
+    // num.toUpperCase(); // This will throw an error because num is a number
+    return num + 3;
+}
+
+/*
+Below will return 13 because the function is treating the argument as a number.
+This is because the function is expecting a type for the argument.
+*/
+
+addThree(10);
+
+export {};
