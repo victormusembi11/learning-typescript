c6397d1c675dcdda19fca8bf627fad5b97e56055
victormusembi11 <victor.musembi11@gmail.com>
2023-08-03 11:19:53 +0300

    typescript data types

--- variables.js
+++ variables.js
@@ -3,3 +3,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var greetings = "Hello, World!";
 // greetings = 10; // This will throw an error because greetings is a string
 var mynum = 10;
+// mynum.toUpperCase(); // This will throw an error because mynum is a number
+// number type
+// ###########
+var userId = 334455;
+// boolean type
+// ############
+var isLoggedIn = true;
--- variables.ts
+++ variables.ts
@@ -7,4 +7,17 @@ let mynum = 10;
 
 // mynum.toUpperCase(); // This will throw an error because mynum is a number
 
+
+// number type
+// ###########
+
+let userId: number = 334455;
+
+// boolean type
+// ############
+
+let isLoggedIn: boolean = true;
+
+// ** Note: Sometimes it's unessesary to specify the type of a variable
+
 export {}; // This is to avoid TS error: "Cannot redeclare block-scoped variable 'greetings'."
