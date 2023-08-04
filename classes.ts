07987c1430b38b72f9983e023f154a7b9c60572a
victormusembi11 <victor.musembi11@gmail.com>
2023-08-04 12:27:52 +0300

    access modifiers

--- classes.ts
+++ classes.ts
@@ -1,8 +1,8 @@
 class User {
 
-    email: string;
-    name: string;
-    city: string = "";
+    public email: string; // setting public access modifier
+    name: string; // by default this is set as public
+    private city: string = "";
     readonly qwerty = 10;
 
     constructor(email: string, name: string) {
@@ -12,4 +12,16 @@ class User {
 }
 
 const user1 = new User("johndoe@example.com", "john doe");
-user1.city = "Nairobi";
+// user1.city = "Nairobi"; // this is private an only accessible in the class
+
+
+class Employee {
+
+    constructor(public email: string, public name: string) { // another way of setting access modifiers
+        this.email = email;
+        this.name = name;
+    }
+
+}
+
+const emp1 = new Employee("janedoe@example.com", "janedoe");
